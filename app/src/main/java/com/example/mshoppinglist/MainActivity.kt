1e0c3ea827c403fa6a76d7c2c00482226690faaa
laura <laura.carolpavani@gmail.com>
2025-05-25 00:36:45 +1000

    Inittial Commit

---
+++ app/src/main/java/com/example/mshoppinglist/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.example.myshoppinglist
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.dialog
+import androidx.navigation.compose.rememberNavController
+import com.example.mshoppinglist.LocationSelectionScreen
+import com.example.mshoppinglist.LocationUtils
+import com.example.mshoppinglist.LocationViewModel
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MaterialTheme {
+                Surface(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(WindowInsets.safeDrawing.asPaddingValues()),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Navigation()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Navigation(){
+    val navController = rememberNavController()
+    val viewModel: LocationViewModel = viewModel()
+    val context = LocalContext.current
+    val locationUtils = LocationUtils(context)
+
+    NavHost(navController, startDestination ="shoppinglistscreen"){
+        composable("shoppinglistscreen"){
+            ShoppingListApp(
+                locationUtils = locationUtils,
+                viewModel = viewModel,
+                navController = navController,
+                context = context,
+                address = viewModel.address.value.firstOrNull()?.formatted_address ?: "No Address"
+            )
+        }
+
+        dialog("locationscreen"){backstack ->
+            viewModel.location.value?.let{it1 ->
+                LocationSelectionScreen(location = it1, onLocationSelected = {locationdata ->
+                    viewModel.fetchAddress("${locationdata.latitude}, ${locationdata.longitude}")
+                    navController.popBackStack()
+                })
+
+            }
+
+        }
+    }
+}
